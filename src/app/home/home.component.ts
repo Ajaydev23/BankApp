b842965d8c65110668a04b3c74bf97d143153f44
ajay <ajaydev2223@gmail.com>
2021-11-24 08:57:55 +0530

    bankApp deposit

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +10,19 @@ export class HomeComponent implements OnInit {
   acno=""
   pswd=""
   amount=""
+  acno1=""
+  pswd1=""
+  amount1=""
 
-  constructor() { }
+  constructor(private ds:DataService) { }
 
   ngOnInit(): void {
   }
   Deposit(){
+    var acno=this.acno
+    var pswd=this.pswd
+    var amount=this.amount
+    let balance=this.ds.[acno].balance
     alert("Amount Deposited.Transaction Successful")
   }
   Withdraw(){
